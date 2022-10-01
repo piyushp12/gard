026632a2e183d35bdcf52d0d6fce9c465d97de83
Piyush <96821378+piyushp12@users.noreply.github.com>
2022-10-01 17:19:54 +0530

    Update settings.py

--- oto/settings.py
+++ oto/settings.py
@@ -83,9 +83,9 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
 #     'default': {
@@ -99,8 +99,8 @@ DATABASES['default'].update(db_from_env)
 # }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+ Password validation
+ https://docs.djangoproject.com/en/4.0/ref/settings/auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
